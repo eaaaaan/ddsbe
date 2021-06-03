59245b36c388eb5b8d313f3bddee9edefa8689f6
eaaaaan <ablaza.mareanerika@gmail.com>
2021-06-03 23:28:43 +0800

    Latest ddsbe codes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,13 +49,8 @@
 
         public function show($id) {
 
-<<<<<<< HEAD
             $user = User::findOrFail($id);
             return $this->successResponse($user);       
-=======
-            $users = User::findOrFail($id);
-            return $this->successResponse($users);       
->>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
 
             //old code
             /*
@@ -70,10 +65,7 @@
                 'username' => 'max:20',
                 'password' => 'max:20',
                 //'admin' => 'in:1,0',
-<<<<<<< HEAD
                 'job_id' => 'required|numeric|min:1|not_in:0',
-=======
->>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
             ];
 
             $this->validate($request, $rules);
@@ -86,7 +78,6 @@
                 return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
             }
 
-<<<<<<< HEAD
             $userjob = UserJob::findOrFail($request->job_id);
             
             $user = User::findOrFail($id);
@@ -100,10 +91,6 @@
 
             $user->save();
             return $this->successResponse($user);
-=======
-            $users->save();
-            return $this->successResponse($users);
->>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
 
             
             //old code
@@ -124,15 +111,9 @@
 
         public function delete($id) {
 
-<<<<<<< HEAD
             $user = User::findOrFail($id);
             $user->delete();
            return $this->successResponse($user);
-=======
-            $users = User::findOrFail($id);
-            $users->delete();
-            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
->>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
             //old code
             /*
             $users = User::where('id', $id)->first();
