209ac25ca8bf5b8ac4b8f3a086391a1e3804a30e
eaaaaan <ablaza.mareanerika@gmail.com>
2021-06-03 23:25:46 +0800

    Latest ddsbe codes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,8 +49,13 @@
 
         public function show($id) {
 
+<<<<<<< HEAD
+            $user = User::findOrFail($id);
+            return $this->successResponse($user);       
+=======
             $users = User::findOrFail($id);
             return $this->successResponse($users);       
+>>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
 
             //old code
             /*
@@ -65,7 +70,10 @@
                 'username' => 'max:20',
                 'password' => 'max:20',
                 //'admin' => 'in:1,0',
+<<<<<<< HEAD
                 'job_id' => 'required|numeric|min:1|not_in:0',
+=======
+>>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
             ];
 
             $this->validate($request, $rules);
@@ -78,8 +86,24 @@
                 return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
             }
 
+<<<<<<< HEAD
+            $userjob = UserJob::findOrFail($request->job_id);
+            
+            $user = User::findOrFail($id);
+
+            $user->fill($request->all());
+
+            //if no changes happen
+            if ($user->isClean()){
+                return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $user->save();
+            return $this->successResponse($user);
+=======
             $users->save();
             return $this->successResponse($users);
+>>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
 
             
             //old code
@@ -100,9 +124,15 @@
 
         public function delete($id) {
 
+<<<<<<< HEAD
+            $user = User::findOrFail($id);
+            $user->delete();
+           return $this->successResponse($user);
+=======
             $users = User::findOrFail($id);
             $users->delete();
             return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+>>>>>>> 9e58d759ebfef80834c71f48804685a320301dfc
             //old code
             /*
             $users = User::where('id', $id)->first();
